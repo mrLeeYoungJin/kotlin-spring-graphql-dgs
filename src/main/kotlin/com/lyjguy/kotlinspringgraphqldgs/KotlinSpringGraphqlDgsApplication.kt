8d2b58eb23de4247ef2468df4c5bbf2bb17f8354
lyjguy <lyjguy@naver.com>
2022-09-19 13:32:30 +0900

    main class 추가

---
+++ src/main/kotlin/com/lyjguy/kotlinspringgraphqldgs/KotlinSpringGraphqlDgsApplication.kt
@@ -0,0 +1,11 @@
+package com.lyjguy.kotlinspringgraphqldgs
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringGraphqlDgsApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinSpringGraphqlDgsApplication>(*args)
+}
